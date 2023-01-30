07a530f68345fd80161b11cc124d50e0e5104555
dgonza18 <dgonza18@gmail.com>
2023-01-30 05:09:40 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+#python
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+#FastAPI
+
 from fastapi import FastAPI
+from fastapi import Body
 
 app= FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name : str
+    age : int
+    hair_color : Optional[str] = None
+    is_marriaged: Optional[bool] = None
+
 
 @app.get('/')
 def home():
     return{'Hello':'Wolrd'}
+
+
+#rquest and rosponse body
+
+@app.post('/person/new')
+
+def create_funtion(persona: Person= Body(...)):
+    return Person
+    
